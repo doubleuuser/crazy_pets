b2d13c7dc0534325ef25d1d111206389d5d02b67
erik b <erik.be@outlook.com>
2022-06-30 21:49:47 +0800

    added new view and backend logic

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -16,12 +16,15 @@ class PetsController < ApplicationController
   def create
     @pet = Pet.new(pet_params)
     # how to get the associations?
+    @pet.user = current_user
+    puts current_user
+    puts "#{@pet.name} is my pet and i am #{@pet.user_id}"
     @pet.save
     redirect_to pets_path(@pet)
   end
 
   def pet_params
-    params.require(:pet).permit(:name, :description, :price, :species)
+    params.require(:pet).permit(:name, :description, :price, :species, :image_url)
   end
 
   def set_pet
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  before_action :set_user
+  def my_pets
+    @my_pets = @user.pets
+  end
+
+  def my_bookings
+    @my_bookings = @user.bookings
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+
+
+end
