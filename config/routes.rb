efa2cbc26fd54fc4bbfcc9132fbfa483b7438ca5
lungi nzima <nzimalungi@gmail.com>
2022-07-05 19:47:00 +0800

    created basic layout for different html

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pets#index'
+  root to: 'pets#home'
 
   resources :pets do
     resources :bookings, only: [ :index, :new, :create ]
